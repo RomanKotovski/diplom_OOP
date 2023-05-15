36295a5248739681ef156042176009927da16fa8
Roman_Kotovski <roman_kotovski@mail.ru>
2023-05-16 01:45:52 +0300

    update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 import json
-from pprint import pprint
-import sys
-import time
+from datetime import datetime
 import yadisk
+import time
+from tqdm.auto import tqdm
 
 
 class VK:
@@ -26,25 +26,34 @@ class Ya:
         self.token = token
         self.file_path = file_path
     def upload_file():
+        yadisk.YaDisk(token=token).mkdir('/photo') #создаем новую папку
         link = VK(token_VK, user_id).users_info()
         data_photo = link['response']['items']
-        count = 0
-        for data in data_photo:
+        count = 0  # счетчик для количества фото, по умолчанию 5
+        name_list = []  # список имен для поиска повторений
+        file_list = []  # итоговый список имен и размеров фото
+        for data in tqdm(data_photo, 'Загрузка фото', total=5):
             if count < 5:
-                time.sleep(1)
-                name_photo = str(data['likes']['count'])
+                time.sleep(0.2)
+                name_photo =  str(data['likes']['count'])
+                if name_photo in name_list:
+                    name_photo += f" {str(datetime.fromtimestamp(data['date']))}" # в случае повторения имени добавляем дату
+                name_list.append(name_photo)
                 sizes_photo = data['sizes']
                 for size in sizes_photo:
-                    if size['type'] == 'w':
+                    if size['type'] == 'w': # w - самый большой размер фото
                         link_photo = size['url']
-                pprint(link_photo)
-                y = yadisk.YaDisk(token=token).upload_url(link_photo, name_photo)
+                        file_list.append({'name': name_photo, 'size': size['type']})
+                yadisk.YaDisk(token=token).upload_url(link_photo, f'/photo/{name_photo}.jpg') #загружаем фото в нужную папку
                 count += 1
             else:
-                break
+                break # при достижении порогового значения прерываем цикл
+        json_list = json.dumps(file_list)
+        with open("file_list.json", "w") as file_info:
+            file_info.write(json_list)
 
-
-token_VK = 'vk1.a.GZCJaTIESuUoq_yF0AATelZlf2xpQkrozlQXRGDQdT2odtDMd7ZtHJuZVgXdiCR646SeLJxJlS9NBuNzufbUPWjiuiyWmMxjZ9HdfCqgN1p2XUdISKVxm8kQZfZzrWrWV3_IDQ4_hB4WcUWKwWztqiQas-4AIxZSOlmIjn0LBKKWm4OWykzUhcvq2RqOJ6-xuyqNZSmHmSE1iipYI8aLRQ'
-user_id = '64870295'
-token = 'y0_AgAAAAAzTKoOAADLWwAAAADirnYaBAZfFooQRhSQlYuCtLWT-UTLq6U'   #токен
-Ya.upload_file()
+if __name__ == '__main__':
+    token_VK = 'vk1.a.GZCJaTIESuUoq_yF0AATelZlf2xpQkrozlQXRGDQdT2odtDMd7ZtHJuZVgXdiCR646SeLJxJlS9NBuNzufbUPWjiuiyWmMxjZ9HdfCqgN1p2XUdISKVxm8kQZfZzrWrWV3_IDQ4_hB4WcUWKwWztqiQas-4AIxZSOlmIjn0LBKKWm4OWykzUhcvq2RqOJ6-xuyqNZSmHmSE1iipYI8aLRQ'
+    user_id = str(input('Введите id пользователя VK: ')) #юсерайди вк
+    token = str(input('Введите ваш токен ЯндексДиск: '))   #токен яндекса
+    Ya.upload_file()
